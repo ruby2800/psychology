98951e53d8e094fe6d0a8d0dad8cb1eea92ea973
余函倩 <you@example.com>
2019-07-04 01:06:21 +0800

    超連結，data toggle在雷

--- routes/food.js
+++ routes/food.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('food/index', { title: '你只是心生病了' });
 });
 
 module.exports = router;
